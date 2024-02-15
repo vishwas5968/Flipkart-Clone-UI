43cd751bac3faea5812881c49bdf3f5720373e60
vishwas5968 <vishwasr968@gmail.com>
2024-02-15 17:01:43 +0530

    otp verification and registration done

--- src/Components/Public/VerifyOTP.jsx
+++ src/Components/Public/VerifyOTP.jsx
@@ -1,7 +1,39 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import axios from "axios";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const VerifyOTP = () => {
+  const [OTP, setotp] = useState();
+  // const [able, setable] = useState(true);
+
+  const navs = useNavigate();
+
+  const otpVerification = async (e) => {
+    // setable(true);
+    const url = "http://localhost:8080/api/v1/verify-otp";
+
+    const body = {
+      email: sessionStorage.getItem("email"),
+      otp: OTP,
+    };
+    const header = {
+      "Content-Type": "application/json",
+    };
+
+    try {
+      const res = await axios.post(url, body, header);
+      console.log(res);
+      if (res.status === 201) {
+        navs("/home");
+      } else {
+        navs("/search");
+        // setable(false);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className=" mt-20 ml-72 w-8/12 h-full justify-center flex items-center ">
       <section className=" w-2/4 h-96 pb-28 drop-shadow-2xl cart border-indigo-600">
@@ -26,15 +58,24 @@ const VerifyOTP = () => {
             </label>
           </div>
           <div className="pl-5 h-12">
-            <input type="text" className="text-xl h-12 w-60 pl-2" autoFocus />
+            <input
+              type="text"
+              className="text-xl h-12 w-60 pl-2"
+              autoFocus
+              onChange={(e) => {
+                setotp(e.target.value);
+              }}
+            />
           </div>
-          <Link to={"/verify-otp"} className="text-black ">
-            <div className="pl-5">
-              <button className="w-60 h-12 bg-amber-400 mt-12 text-xl">
-                Continue
-              </button>
-            </div>
-          </Link>
+          <div className="pl-5">
+            <button
+              className="w-60 h-12 bg-amber-400 mt-12 text-xl hover:bg-amber-700"
+              onClick={otpVerification}
+              // disabled={able}
+            >
+              Continue
+            </button>
+          </div>
         </div>
       </section>
     </div>
